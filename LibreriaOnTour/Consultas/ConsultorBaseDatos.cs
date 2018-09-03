0b7993b5fc2c5f38f4f5833e317b8110c2ae9bcd
Rolando Andrés Aburto Olivares <raburtolivares@gmail.com>
2018-09-03 18:20:37 -0300

    Nuevos cambios
    Trata de ver yaya

--- LibreriaOnTour/Consultas/ConsultorBaseDatos.cs
+++ LibreriaOnTour/Consultas/ConsultorBaseDatos.cs
@@ -8,18 +8,56 @@ namespace LibreriaOnTour.Consultas
 {
     public class ConsultorBaseDatos
     {
-        
-        public bool autenticar(Usuario usuario)
+        public List<Usuario> GetUsuarios()
         {
-            String commandText = "SELECT * FROM USUARIO"
+            List<Usuario> listado = new List<Usuario>();
+
+            String commandText = "SELECT NOMBRE_USUARIO, PASSWORD FROM USUARIO";
+
+            Usuario usuario = new Usuario();
             using (OracleConnection conector = Conexion.Conexion.Conectar())
             {
                 conector.Open();
 
                 OracleCommand command = new OracleCommand(commandText, conector);
+                OracleDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    usuario.nombre = Convert.ToString(reader["NOMBRE_USUARIO"]);
+                    usuario.password = Convert.ToString(reader["PASSWORD"]);
+                    //usuario.tipoUsuario.id = Convert.ToInt32(reader["ID_TIPO_USUARIO"]);
+                    //usuario.tipoUsuario.nombre = Convert.ToString(reader["NOMBRE_TIPO_USUARIO"]);
+
+                    listado.Add(usuario);
+                }
             }
             
-            return false;
+            return listado;
+        }
+
+
+        public bool Validar(Usuario usuario)
+        {
+            bool estado = false;
+            List<Usuario> usuarios = null;
+            try
+            {
+                usuarios = GetUsuarios();
+
+                foreach (var u in usuarios)
+                {
+                    if (u.nombre == usuario.nombre && u.password == usuario.password) 
+                    {
+                        estado = true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return estado;
         }
     }
 }
---
+++ Prueba/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibreriaOnTour.Consultas;
+
+namespace Prueba
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /**Usuario usuario = new Usuario();
+            //TipoUsuario tipoUsuario = new TipoUsuario();
+            usuario.nombre = "ev_001";
+            usuario.password = "ev_001";
+            //tipoUsuario.id = 1;
+            //tipoUsuario.nombre = "Ejecutivo_Ventas";
+            //usuario.tipoUsuario = tipoUsuario;
+            ConsultorBaseDatos consultor = new ConsultorBaseDatos();
+
+            if (consultor.Validar(usuario))
+            {
+                Console.WriteLine("Bienvenido");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        */
+
+            foreach (var item in new ConsultorBaseDatos().GetUsuarios())
+            {
+                Console.WriteLine(item.nombre);
+            }
+            Console.ReadKey();
+        }
+    }
+}
